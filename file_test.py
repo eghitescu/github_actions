6c69a2a8a6a6796857ffad9db6d6abb3a3a8b7cf
eghitescu <eghitescu@yahoo.com>
2024-06-11 11:37:48 +0000

    erreur provoquée dans file_test.py

--- file_test.py
+++ file_test.py
@@ -7,7 +7,7 @@ def test_calc_addition():
 def test_calc_substraction():
     # Fonction test du résultat de 2-4
     output = 2 - 4
-    assert output == -2
+    assert output == -6
 
 
 def test_calc_multiply():
